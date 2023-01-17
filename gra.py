d38456c7053fe10c43b2d7a1e71e2c98f819f298
Eryk Kawczynski <hwscxv@gmail.com>
2023-01-17 21:48:01 +0100

    pierwszy plik gra.py

---
+++ gra.py
@@ -0,0 +1,41 @@
+import sys
+
+
+
+no_of_tries  = 5
+word = 'sowa'
+used_letters = []
+user_word = []
+
+
+def find_indexes(word, letter):
+    indexes = []
+    for index, letter_in_word in enumerate(word):
+        if letter_in_word == letter:
+            indexes.append(index)
+    return indexes
+
+
+
+for _ in word:
+    user_word.append('_')
+print("gra wisielec, odgadnij haslo ")
+while True:
+    print(user_word)
+    letter = input('podaj litere ')
+    found_indexes = find_indexes(word, letter)
+    
+    if len(found_indexes) == 0:
+        print('nieprawidlowa litera, utrata zycia.')
+        no_of_tries -= 1
+        if no_of_tries == 0:
+            print('utrata wszystkich zyc, koniec gry')
+            sys.exit(0)
+
+    for index in found_indexes:
+        user_word[index] = letter
+
+    if "".join(user_word) == word:
+        print('brawo wygrana')
+        sys.exit(0)
+    
