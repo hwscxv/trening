68d862a12e905267ad3646c7bdbba1412f334747
Eryk Kawczynski <hwscxv@gmail.com>
2023-02-18 01:26:36 +0100

    dodaje database.py, modyfikuje yt_01.py

--- trening/yt_01.py
+++ trening/yt_01.py
@@ -1,31 +1,41 @@
 from sys import argv
-import sqlite3
-from os import getenv
-from dotenv import load_dotenv
-load_dotenv()
+from  database import Database                               
+from os import getenv            #getenv 
+from dotenv import load_dotenv   #getenv                 
+load_dotenv()           #getenv
 
-
-class Database:
-    def __init__(self, database_name):
-        self.connection = sqlite3.connect(database_name)          #polaczenie z baza danych przez modul sqlite3
-        self.cursor = self.connection.cursor()                    # kurson
-    
-    def __del__(self):
-        self.connection.close()
-    
-    def create_table(self, sql: str):
-        self.cursor.execute(sql)
-        self.connection.commit()
-
-
-
-
-if len(argv) > 1 and argv[1] == "setup":    #jezeli argv zwraca wiecej niz 1 i druga pozycja to setup
-    print('tworze baze danych')
+if len(argv) > 1 and argv[1] == "setup":
+    '''
+    initialize database 
+    usage python main.py setup
+    '''
+    #jezeli argv zwraca wiecej niz 1 i druga pozycja to setup
+    print('tworze  tabele w bazie danych')
     db = Database(getenv('DB-NAME')) 
-    db.create_table('''CREATE TABLE urls (id INTEGER PRIMARY KEY AUTOINCREMENT, category TEXT, url TEXT)''')        #tworzenie tabeli w db
-
-if len(argv) == 4 and argv[2] == 'add':
+    db.create_table('''CREATE TABLE urls (id INTEGER PRIMARY KEY AUTOINCREMENT, category TEXT, url TEXT)''')        #tworzenie tabeli w d
+    #                              nazwa ( kolumna 1                          , kolumna 2    , kolumna 3   )
+if len(argv) == 4 and argv[1] == 'add':
+    '''
+        adding new resource with category
+        usage: python yt_01.py add przyklady http//przyklad.pl
+                            1    2         3                 4 
+    '''
     print('dodaje nowy url')
+    category = argv[2]
+    url = argv[3]
+    db = Database(getenv('DB-NAME'))
+    db.insert('urls', None, category, url)
 
+if len(argv) == 3  and argv[1] == 'list':
+    '''
+        usage: python main.py list category_name
+    '''
 
+    print('lista linkow z kategorii: ')
+    category = argv[1]
+    db = Database(getenv('DB-NAME'))
+    links = db.fetch_all('urls', category=category)
+#                 nazwa tabeli  
+ 
+    for link in links:
+        print(links)
